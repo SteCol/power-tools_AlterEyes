679e77eee6e23cc0689e68727a7627033fa01cc4
Stef Colruyt <stef.colruyt@altereyes.com>
2020-07-09 10:57:00 +0200

    Added resolution to RDP_Mule

--- RDP_Mule.ps1
+++ RDP_Mule.ps1
@@ -1,6 +1,12 @@
-mstsc /v:10.0.10.11
-mstsc /v:10.0.10.21
-mstsc /v:10.0.10.22
-mstsc /v:10.0.10.23
-mstsc /v:10.0.10.24
+$width = 800
+$height = 640
+
+# mstsc /v:10.0.11.107 /w:$width /h:$height
+# Pause
+
+mstsc /v:10.0.10.11 /w:$width /h:$height
+mstsc /v:10.0.10.21 /w:$width /h:$height
+mstsc /v:10.0.10.22 /w:$width /h:$height
+mstsc /v:10.0.10.23 /w:$width /h:$height
+mstsc /v:10.0.10.24 /w:$width /h:$height
 
