f87b325f3f3a4f3773f8518fe15e3f301ba26b02
Stef Colruyt <stef.colruyt@altereyes.com>
2020-11-12 15:47:18 +0100

    added w and h to RDP_Dojo.rdp

--- RDP_Dojo.ps1
+++ RDP_Dojo.ps1
@@ -1,6 +1,9 @@
-mstsc /v:dojoserv-001
-mstsc /v:dojogame-001
-mstsc /v:dojogame-002
-mstsc /v:dojogame-003
-mstsc /v:dojogame-004
+$width = 800
+$height = 640
+
+mstsc /v:dojoserv-001  /w:$width /h:$height
+mstsc /v:dojogame-001 /w:$width /h:$height
+mstsc /v:dojogame-002 /w:$width /h:$height
+mstsc /v:dojogame-003 /w:$width /h:$height
+mstsc /v:dojogame-004 /w:$width /h:$height
 
